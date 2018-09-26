13e1667049f0f6d5c772714afb46c578a7408c3e
James Taylor <james.taylor2@comcast.net>
2018-09-26 14:52:50 -0400

    added in variables for constraint, etc.

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,14 @@
+import java.util.ArrayList;
+import java.util.HashMap;
+
 public class Main {
     public static void main(String [ ] args){
-
+        HashMap<String, Integer> items;
+        HashMap<String, Integer> bags;
+        int minItems;
+        int maxItems;
+        ArrayList<Unary> unaries;
+        ArrayList<Binary> binaries;
+        ArrayList<Mutual> mutuals;
     }
 }
